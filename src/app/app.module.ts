6725c151105f32dba15340fb65b66d9154859b9c
Nyambura Mwangi <mwanginyambura19@gmail.com>
2019-03-28 10:20:59 +0300

    import files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {QuotesComponent } from './quotes/quotes.component';
+import {FormsComponent} from './forms/forms.component'
+import { DetailsComponent} from './details/details.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    FormsComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
