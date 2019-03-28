c4e7d71ce7126b2dd18bc9223e57331121eb5e08
Nyambura Mwangi <mwanginyambura19@gmail.com>
2019-03-28 11:18:41 +0300

    deployed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,19 @@ import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {QuotesComponent } from './quotes/quotes.component';
-import {FormsComponent} from './forms/forms.component'
-import { DetailsComponent} from './details/details.component'
+import {FormsComponent} from './forms/forms.component';
+import { DetailsComponent} from './details/details.component';
+import { AppHighlightDirective } from './app-highlight.directive';
+import { CountPipePipe } from './count-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     FormsComponent,
-    DetailsComponent
+    DetailsComponent,
+    AppHighlightDirective,
+    CountPipePipe
   ],
   imports: [
     BrowserModule,
